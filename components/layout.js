713f7da84eca1d0efd74574e18429da974ea38f1
Andrew Puig <andrew@shethinx.com>
2021-01-22 09:37:23 -0500

    rm componennts

--- components/layout.js
+++
@@ -1,8 +0,0 @@
-import styles from './Layout.module.css'
-
-/*
-The Layout component is used to wrap pages to maintain a consistent styling.
-*/
-export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>
-}
